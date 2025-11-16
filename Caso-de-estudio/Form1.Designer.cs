5937a4a80f7a8d88aea79a6fb768173e7cebef37
Kevinech <kevinemch@uamv.edu.ni>
2025-11-15 21:32:51 -0600

    Se agregaron las funcionalidades de los botones agregar, eliminar y buscar

---
+++ Caso-de-estudio/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Caso_de_estudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvPuestos = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgregarP = new System.Windows.Forms.Button();
+            this.tbPuesto = new System.Windows.Forms.TextBox();
+            this.btnBusquedaP = new System.Windows.Forms.Button();
+            this.btnConteoP = new System.Windows.Forms.Button();
+            this.btnNiveles = new System.Windows.Forms.Button();
+            this.btnEliminarP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvPuestos
+            // 
+            this.tvPuestos.Location = new System.Drawing.Point(119, 133);
+            this.tvPuestos.Name = "tvPuestos";
+            this.tvPuestos.Size = new System.Drawing.Size(413, 355);
+            this.tvPuestos.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puesto:";
+            // 
+            // btnAgregarP
+            // 
+            this.btnAgregarP.Location = new System.Drawing.Point(553, 101);
+            this.btnAgregarP.Name = "btnAgregarP";
+            this.btnAgregarP.Size = new System.Drawing.Size(75, 41);
+            this.btnAgregarP.TabIndex = 2;
+            this.btnAgregarP.Text = "Agregar";
+            this.btnAgregarP.UseVisualStyleBackColor = true;
+            this.btnAgregarP.Click += new System.EventHandler(this.btnAgregarP_Click);
+            // 
+            // tbPuesto
+            // 
+            this.tbPuesto.Location = new System.Drawing.Point(119, 101);
+            this.tbPuesto.Name = "tbPuesto";
+            this.tbPuesto.Size = new System.Drawing.Size(413, 26);
+            this.tbPuesto.TabIndex = 3;
+            // 
+            // btnBusquedaP
+            // 
+            this.btnBusquedaP.Location = new System.Drawing.Point(553, 191);
+            this.btnBusquedaP.Name = "btnBusquedaP";
+            this.btnBusquedaP.Size = new System.Drawing.Size(75, 39);
+            this.btnBusquedaP.TabIndex = 4;
+            this.btnBusquedaP.Text = "Busqueda";
+            this.btnBusquedaP.UseVisualStyleBackColor = true;
+            this.btnBusquedaP.Click += new System.EventHandler(this.btnBusquedaP_Click);
+            // 
+            // btnConteoP
+            // 
+            this.btnConteoP.Location = new System.Drawing.Point(553, 284);
+            this.btnConteoP.Name = "btnConteoP";
+            this.btnConteoP.Size = new System.Drawing.Size(75, 44);
+            this.btnConteoP.TabIndex = 5;
+            this.btnConteoP.Text = "Conteo";
+            this.btnConteoP.UseVisualStyleBackColor = true;
+            // 
+            // btnNiveles
+            // 
+            this.btnNiveles.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnNiveles.Location = new System.Drawing.Point(553, 236);
+            this.btnNiveles.Name = "btnNiveles";
+            this.btnNiveles.Size = new System.Drawing.Size(75, 42);
+            this.btnNiveles.TabIndex = 6;
+            this.btnNiveles.Text = "Niveles";
+            this.btnNiveles.UseVisualStyleBackColor = true;
+            this.btnNiveles.Click += new System.EventHandler(this.btnNiveles_Click);
+            // 
+            // btnEliminarP
+            // 
+            this.btnEliminarP.Location = new System.Drawing.Point(553, 148);
+            this.btnEliminarP.Name = "btnEliminarP";
+            this.btnEliminarP.Size = new System.Drawing.Size(75, 37);
+            this.btnEliminarP.TabIndex = 7;
+            this.btnEliminarP.Text = "Eliminar";
+            this.btnEliminarP.UseVisualStyleBackColor = true;
+            this.btnEliminarP.Click += new System.EventHandler(this.btnEliminarP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnEliminarP);
+            this.Controls.Add(this.btnNiveles);
+            this.Controls.Add(this.btnConteoP);
+            this.Controls.Add(this.btnBusquedaP);
+            this.Controls.Add(this.tbPuesto);
+            this.Controls.Add(this.btnAgregarP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tvPuestos);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Jerarquía Organizativa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvPuestos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregarP;
+        private System.Windows.Forms.TextBox tbPuesto;
+        private System.Windows.Forms.Button btnBusquedaP;
+        private System.Windows.Forms.Button btnConteoP;
+        private System.Windows.Forms.Button btnNiveles;
+        private System.Windows.Forms.Button btnEliminarP;
+    }
+}
+
---
+++ Caso-de-estudio/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caso_de_estudio
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Handles the click event of the "Agregar" button to add a new node to the tree view.
+        /// </summary>
+        /// <remarks>If a node is selected in the tree view, this method adds a child node with the text
+        /// from the textbox. If no nodes exist in the tree view, it creates a root node with the text from the textbox.
+        /// If a root node already exists, a warning message is displayed to prevent adding multiple root nodes. The
+        /// tree view is expanded after the operation to display all nodes.</remarks>
+        /// <param name="sender">The source of the event, typically the "Agregar" button.</param>
+        /// <param name="e">The event data associated with the click event.</param>
+        private void btnAgregarP_Click(object sender, EventArgs e)
+        {
+            //Agrega un nodo hijo a la raiz (es la que se selecciona)
+            if(tvPuestos.SelectedNode != null)
+            {
+                tvPuestos.SelectedNode.Nodes.Add(tbPuesto.Text);
+            }
+            //Crea un nodo raiz
+            if (tvPuestos.Nodes.Count == 0)
+            {
+                //Agrega nodo con el nombre / puesto escrito en el textbox
+                tvPuestos.Nodes.Add(tbPuesto.Text);
+
+            }
+            else
+            {
+                //Manda una alerta cuando el usuario quiere crear otra raíz
+                MessageBox.Show("Solo puede agregar una raíz", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+            //Expande el arbol para poder mostrar todos los elementos
+            tvPuestos.ExpandAll();
+
+
+
+        }
+
+        /// <summary>
+        /// Handles the click event of the "Eliminar" button, removing the selected node from the tree view.
+        /// </summary>
+        /// <remarks>This method ensures that the root node cannot be deleted. If the selected node is a
+        /// root node, a warning message is displayed, and no action is taken. If the selected node has child nodes,
+        /// they are also removed along with the selected node.</remarks>
+        /// <param name="sender">The source of the event, typically the "Eliminar" button.</param>
+        /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnEliminarP_Click(object sender, EventArgs e)
+        {
+            //
+            if(tvPuestos.SelectedNode  != null)
+            {
+                //
+                if (tvPuestos.Nodes.Count > 0)
+                {
+                    //se crea una variable donde especifica que los nodos padres no se pueden eliminar o dejar de existir
+                    var nodo = tvPuestos.SelectedNode;
+                    if (nodo.Parent == null)
+                    {
+
+                        MessageBox.Show("No puedes eliminar la raíz", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+
+                    }
+                    //elimina los nodos hijos
+                    nodo.Remove();
+
+                }
+
+            }
+
+
+
+        }
+
+
+        /// <summary>
+        /// Handles the click event of the "Buscar" button, searching for a node in the tree view that matches the text
+        /// entered in the associated text box.
+        /// </summary>
+        /// <remarks>If a matching node is found, it is selected in the tree view, and a message box is
+        /// displayed indicating the node's text. If no matching node is found, a message box notifies the
+        /// user.</remarks>
+        /// <param name="sender">The source of the event, typically the "Buscar" button.</param>
+        /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+        private void btnBusquedaP_Click(object sender, EventArgs e)
+        {
+            //especifica que la variable "buscar" es el texto puesto en el textbox
+            string buscar = tbPuesto.Text.ToString();
+
+            //Llama el metodo de buscar nodo
+            TreeNode encontrado = GetBuscar(tvPuestos.Nodes, buscar);
+
+            //
+            if (encontrado != null)
+            {
+                tvPuestos.SelectedNode = encontrado;
+                MessageBox.Show("Puesto encontrado: " + encontrado.Text + ".", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+
+            }
+            //
+            else
+            {
+                MessageBox.Show("Puesto no encontrado.");
+            }
+
+        }
+
+
+        /// <summary>
+        /// Searches for a <see cref="TreeNode"/> with the specified text within the given collection of nodes,
+        /// including their child nodes, using a case-insensitive comparison.
+        /// </summary>
+        /// <remarks>This method performs a recursive search, traversing the child nodes of each node in
+        /// the collection.</remarks>
+        /// <param name="nodos">The collection of <see cref="TreeNode"/> objects to search.</param>
+        /// <param name="texto">The text to search for within the nodes.</param>
+        /// <returns>The first <see cref="TreeNode"/> that matches the specified text, or <see langword="null"/> if no matching
+        /// node is found.</returns>
+        TreeNode GetBuscar(TreeNodeCollection nodos, string texto)
+        {
+
+            foreach (TreeNode nodo in nodos)
+            {
+                //Ignora mayúsculas y minúsculas
+                if (nodo.Text.Equals(texto, StringComparison.OrdinalIgnoreCase))
+                    return nodo;  //Detiene la busqueda si encuentra el nodo
+
+                //Busca recursivamente en los hijos
+                TreeNode encontrado = GetBuscar(nodo.Nodes, texto);
+                if (encontrado != null)
+                    return encontrado;
+
+
+            }
+            //Si no encuentra ningun nodo con el mismo nombre del tbNodo
+            return null;
+
+
+
+
+        }
+
+        private void btnNiveles_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+
+
+
+
+
+}
