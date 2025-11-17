c1d6102ffdd19a88bdf084260dabb1774c4a6182
Kevinech <kevinemch@uamv.edu.ni>
2025-11-16 22:36:48 -0600

    Se actualizo la funcionalidad de agregar nodos y se agrego la funcion de unir nodos

--- Caso-de-estudio/Clases/Nodos.cs
+++ Caso-de-estudio/Clases/Nodos.cs
@@ -22,12 +22,36 @@ namespace Caso_de_estudio.Clases
         // public Dictionary<Nodo, int> ConexionesConPeso = new Dictionary<Nodo, int>();
 
 
+        public class Grafo
+        {
+            public List<Nodo> Nodos = new List<Nodo>();
+
+            // Añadir nodo
+            public Nodo GetAgregarNodo(string nombre)
+            {
+                var existe = Nodos.FirstOrDefault(n => n.Nombre == nombre);
+                if (existe != null) return existe;
+
+                var nuevo = new Nodo(nombre);
+                Nodos.Add(nuevo);
+                return nuevo;
+            }
+
+            // Conectar dos nodos
+            public void GetConectar(string nombre1, string nombre2)
+            {
+                var nodoA = GetAgregarNodo(nombre1);
+                var nodoB = GetAgregarNodo(nombre2);
+
+                if (!nodoA.Conexiones.Contains(nodoB)) nodoA.Conexiones.Add(nodoB);
+                if (!nodoB.Conexiones.Contains(nodoA)) nodoB.Conexiones.Add(nodoA);
+            }
 
 
-        
+
+        }
 
     }
-
 
 
 
