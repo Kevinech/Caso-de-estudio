51697335a07223487a18ce87243f6df9d4e962de
Kevinech <kevinemch@uamv.edu.ni>
2025-11-16 18:39:57 -0600

    Se ordeno el diseño del form para que fuese más agradable a la vista, ademas de agregar dos group box para los botones

--- Caso-de-estudio/Form1.Designer.cs
+++ Caso-de-estudio/Form1.Designer.cs
@@ -40,11 +40,15 @@
             this.btnRecorridoP = new System.Windows.Forms.Button();
             this.btnRecorridoI = new System.Windows.Forms.Button();
             this.btnRecorridoO = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tvPuestos
             // 
-            this.tvPuestos.Location = new System.Drawing.Point(119, 133);
+            this.tvPuestos.Location = new System.Drawing.Point(75, 148);
             this.tvPuestos.Name = "tvPuestos";
             this.tvPuestos.Size = new System.Drawing.Size(413, 355);
             this.tvPuestos.TabIndex = 0;
@@ -52,7 +56,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 101);
+            this.label1.Location = new System.Drawing.Point(6, 116);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 20);
             this.label1.TabIndex = 1;
@@ -60,9 +64,9 @@
             // 
             // btnAgregarP
             // 
-            this.btnAgregarP.Location = new System.Drawing.Point(553, 101);
+            this.btnAgregarP.Location = new System.Drawing.Point(6, 14);
             this.btnAgregarP.Name = "btnAgregarP";
-            this.btnAgregarP.Size = new System.Drawing.Size(75, 41);
+            this.btnAgregarP.Size = new System.Drawing.Size(88, 41);
             this.btnAgregarP.TabIndex = 2;
             this.btnAgregarP.Text = "Agregar";
             this.btnAgregarP.UseVisualStyleBackColor = true;
@@ -70,36 +74,37 @@
             // 
             // tbPuesto
             // 
-            this.tbPuesto.Location = new System.Drawing.Point(119, 101);
+            this.tbPuesto.Location = new System.Drawing.Point(75, 116);
             this.tbPuesto.Name = "tbPuesto";
             this.tbPuesto.Size = new System.Drawing.Size(413, 26);
             this.tbPuesto.TabIndex = 3;
             // 
             // btnBusquedaP
             // 
-            this.btnBusquedaP.Location = new System.Drawing.Point(553, 191);
+            this.btnBusquedaP.Location = new System.Drawing.Point(6, 104);
             this.btnBusquedaP.Name = "btnBusquedaP";
-            this.btnBusquedaP.Size = new System.Drawing.Size(75, 39);
+            this.btnBusquedaP.Size = new System.Drawing.Size(88, 39);
             this.btnBusquedaP.TabIndex = 4;
-            this.btnBusquedaP.Text = "Busqueda";
+            this.btnBusquedaP.Text = "Buscar";
             this.btnBusquedaP.UseVisualStyleBackColor = true;
             this.btnBusquedaP.Click += new System.EventHandler(this.btnBusquedaP_Click);
             // 
             // btnConteoP
             // 
-            this.btnConteoP.Location = new System.Drawing.Point(553, 284);
+            this.btnConteoP.Location = new System.Drawing.Point(6, 197);
             this.btnConteoP.Name = "btnConteoP";
-            this.btnConteoP.Size = new System.Drawing.Size(75, 44);
+            this.btnConteoP.Size = new System.Drawing.Size(88, 44);
             this.btnConteoP.TabIndex = 5;
             this.btnConteoP.Text = "Conteo";
             this.btnConteoP.UseVisualStyleBackColor = true;
+            this.btnConteoP.Click += new System.EventHandler(this.btnConteoP_Click);
             // 
             // btnNiveles
             // 
             this.btnNiveles.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.btnNiveles.Location = new System.Drawing.Point(553, 236);
+            this.btnNiveles.Location = new System.Drawing.Point(6, 149);
             this.btnNiveles.Name = "btnNiveles";
-            this.btnNiveles.Size = new System.Drawing.Size(75, 42);
+            this.btnNiveles.Size = new System.Drawing.Size(88, 42);
             this.btnNiveles.TabIndex = 6;
             this.btnNiveles.Text = "Niveles";
             this.btnNiveles.UseVisualStyleBackColor = true;
@@ -107,9 +112,9 @@
             // 
             // btnEliminarP
             // 
-            this.btnEliminarP.Location = new System.Drawing.Point(553, 148);
+            this.btnEliminarP.Location = new System.Drawing.Point(6, 61);
             this.btnEliminarP.Name = "btnEliminarP";
-            this.btnEliminarP.Size = new System.Drawing.Size(75, 37);
+            this.btnEliminarP.Size = new System.Drawing.Size(88, 37);
             this.btnEliminarP.TabIndex = 7;
             this.btnEliminarP.Text = "Eliminar";
             this.btnEliminarP.UseVisualStyleBackColor = true;
@@ -119,16 +124,16 @@
             // 
             this.lbOrden.FormattingEnabled = true;
             this.lbOrden.ItemHeight = 20;
-            this.lbOrden.Location = new System.Drawing.Point(654, 133);
+            this.lbOrden.Location = new System.Drawing.Point(734, 116);
             this.lbOrden.Name = "lbOrden";
             this.lbOrden.Size = new System.Drawing.Size(377, 344);
             this.lbOrden.TabIndex = 9;
             // 
             // btnRecorridoP
             // 
-            this.btnRecorridoP.Location = new System.Drawing.Point(654, 101);
+            this.btnRecorridoP.Location = new System.Drawing.Point(3, 15);
             this.btnRecorridoP.Name = "btnRecorridoP";
-            this.btnRecorridoP.Size = new System.Drawing.Size(89, 26);
+            this.btnRecorridoP.Size = new System.Drawing.Size(97, 40);
             this.btnRecorridoP.TabIndex = 10;
             this.btnRecorridoP.Text = "Preorden";
             this.btnRecorridoP.UseVisualStyleBackColor = true;
@@ -136,9 +141,9 @@
             // 
             // btnRecorridoI
             // 
-            this.btnRecorridoI.Location = new System.Drawing.Point(804, 101);
+            this.btnRecorridoI.Location = new System.Drawing.Point(3, 61);
             this.btnRecorridoI.Name = "btnRecorridoI";
-            this.btnRecorridoI.Size = new System.Drawing.Size(75, 26);
+            this.btnRecorridoI.Size = new System.Drawing.Size(97, 37);
             this.btnRecorridoI.TabIndex = 11;
             this.btnRecorridoI.Text = "Inorden";
             this.btnRecorridoI.UseVisualStyleBackColor = true;
@@ -146,35 +151,55 @@
             // 
             // btnRecorridoO
             // 
-            this.btnRecorridoO.Location = new System.Drawing.Point(936, 98);
+            this.btnRecorridoO.Location = new System.Drawing.Point(4, 104);
             this.btnRecorridoO.Name = "btnRecorridoO";
-            this.btnRecorridoO.Size = new System.Drawing.Size(95, 29);
+            this.btnRecorridoO.Size = new System.Drawing.Size(95, 39);
             this.btnRecorridoO.TabIndex = 12;
             this.btnRecorridoO.Text = "Postorden";
             this.btnRecorridoO.UseVisualStyleBackColor = true;
             this.btnRecorridoO.Click += new System.EventHandler(this.btnRecorridoO_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConteoP);
+            this.groupBox1.Controls.Add(this.btnAgregarP);
+            this.groupBox1.Controls.Add(this.btnBusquedaP);
+            this.groupBox1.Controls.Add(this.btnNiveles);
+            this.groupBox1.Controls.Add(this.btnEliminarP);
+            this.groupBox1.Location = new System.Drawing.Point(494, 148);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(108, 250);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRecorridoP);
+            this.groupBox2.Controls.Add(this.btnRecorridoI);
+            this.groupBox2.Controls.Add(this.btnRecorridoO);
+            this.groupBox2.Location = new System.Drawing.Point(625, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(103, 250);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.btnRecorridoO);
-            this.Controls.Add(this.btnRecorridoI);
-            this.Controls.Add(this.btnRecorridoP);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lbOrden);
-            this.Controls.Add(this.btnEliminarP);
-            this.Controls.Add(this.btnNiveles);
-            this.Controls.Add(this.btnConteoP);
-            this.Controls.Add(this.btnBusquedaP);
             this.Controls.Add(this.tbPuesto);
-            this.Controls.Add(this.btnAgregarP);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tvPuestos);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Jerarquía Organizativa";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,6 +219,8 @@
         private System.Windows.Forms.Button btnRecorridoP;
         private System.Windows.Forms.Button btnRecorridoI;
         private System.Windows.Forms.Button btnRecorridoO;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- Caso-de-estudio/Form1.cs
+++ Caso-de-estudio/Form1.cs
@@ -19,18 +19,10 @@ namespace Caso_de_estudio
 
         }
 
-        /// <summary>
-        /// Handles the click event of the "Agregar" button to add a new node to the tree view.
-        /// </summary>
-        /// <remarks>If a node is selected in the tree view, this method adds a child node with the text
-        /// from the textbox. If no nodes exist in the tree view, it creates a root node with the text from the textbox.
-        /// If a root node already exists, a warning message is displayed to prevent adding multiple root nodes. The
-        /// tree view is expanded after the operation to display all nodes.</remarks>
-        /// <param name="sender">The source of the event, typically the "Agregar" button.</param>
-        /// <param name="e">The event data associated with the click event.</param>
+        
         private void btnAgregarP_Click(object sender, EventArgs e)
         {
-           
+
             // Si hay un nodo seleccionado, se agrega como hijo
             if (tvPuestos.SelectedNode != null)
             {
@@ -54,24 +46,10 @@ namespace Caso_de_estudio
         }
 
 
-        
-
-
-
-        
-
-        /// <summary>
-        /// Handles the click event of the "Eliminar" button, removing the selected node from the tree view.
-        /// </summary>
-        /// <remarks>This method ensures that the root node cannot be deleted. If the selected node is a
-        /// root node, a warning message is displayed, and no action is taken. If the selected node has child nodes,
-        /// they are also removed along with the selected node.</remarks>
-        /// <param name="sender">The source of the event, typically the "Eliminar" button.</param>
-        /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnEliminarP_Click(object sender, EventArgs e)
         {
             //
-            if(tvPuestos.SelectedNode  != null)
+            if (tvPuestos.SelectedNode != null)
             {
                 //
                 if (tvPuestos.Nodes.Count > 0)
@@ -97,15 +75,7 @@ namespace Caso_de_estudio
         }
 
 
-        /// <summary>
-        /// Handles the click event of the "Buscar" button, searching for a node in the tree view that matches the text
-        /// entered in the associated text box.
-        /// </summary>
-        /// <remarks>If a matching node is found, it is selected in the tree view, and a message box is
-        /// displayed indicating the node's text. If no matching node is found, a message box notifies the
-        /// user.</remarks>
-        /// <param name="sender">The source of the event, typically the "Buscar" button.</param>
-        /// <param name="e">An <see cref="EventArgs"/> instance containing the event data.</param>
+       
         private void btnBusquedaP_Click(object sender, EventArgs e)
         {
             //especifica que la variable "buscar" es el texto puesto en el textbox
@@ -131,15 +101,7 @@ namespace Caso_de_estudio
         }
 
 
-        /// <summary>
-        /// Searches for a <see cref="TreeNode"/> with the specified text within the given collection of nodes,
-        /// including their child nodes, using a case-insensitive comparison.
-        /// </summary>
-        /// <remarks>This method performs a recursive search, traversing the child nodes of each node in
-        /// the collection.</remarks>
-        /// <param name="nodos">The collection of <see cref="TreeNode"/> objects to search.</param>
-        /// <param name="texto">The text to search for within the nodes.</param>
-        /// <returns>The first <see cref="TreeNode"/> that matches the specified text, or <see langword="null"/> if no matching
+
         /// node is found.</returns>
         TreeNode GetBuscar(TreeNodeCollection nodos, string texto)
         {
@@ -264,15 +226,20 @@ namespace Caso_de_estudio
         private void btnNiveles_Click(object sender, EventArgs e)
         {
 
+            
+
 
 
         }
 
-       
+        
+
+
+        private void btnConteoP_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        
     }
-
-
-
-
-
 }
